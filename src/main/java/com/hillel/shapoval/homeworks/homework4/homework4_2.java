5e6c4ea69971783824b3fcc547fe941305ff4f63
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-10-24 23:26:57 +0300

    homework4

--- src/main/java/com/hillel/shapoval/homeworks/homework4/homework4_2.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework4/homework4_2.java
@@ -5,9 +5,9 @@ public class homework4_2 {
     public static void main(String[] args) {
 
 
-        int a = 5;
-        int b = 6;
-        int c = 8;
+        int a = 10;
+        int b = 12;
+        int c = 16;
         int volume = a * b * c;
         System.out.println("Об'єм паралелепіпеда = " + volume);
 
