4fd5ec7b20067c7866b0e08f3b5e20716f5a3759
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-10-24 16:30:23 +0300

    commit all files

--- src/main/java/com/hillel/shapoval/homeworks/homework3/Main.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework3/Main.java
@@ -3,17 +3,16 @@ package com.hillel.shapoval.homeworks.homework3;
 public class Main {
     public static void main(String[] args) {
 
-//        double latitude = 50.27;
-//        double longitude = 30.31;
-//        String name = "довгота=";
-//        String name1 = "широта=";
-//        String name2 = "Майдан незалежності";
-//        char degrees = '\u00B0';
-//
-//
-//        System.out.println(name2);
-//        System.out.println(name + latitude + degrees);
-//        System.out.println(name1 + longitude + degrees);
+       double latitude = 50.27;
+       double longitude = 30.31;
+       String name = "довгота=";
+       String name1 = "широта=";
+       String name2 = "Майдан незалежності";
+       char degrees = '\u00B0';
+
+       System.out.println(name2);
+       System.out.println(name + latitude + degrees);
+       System.out.println(name1 + longitude + degrees);
 
 
         System.out.println();
