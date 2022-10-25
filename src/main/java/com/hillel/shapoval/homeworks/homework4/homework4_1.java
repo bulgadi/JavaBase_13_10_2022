3b19898ebfe438e2c37d2dee7d30fc7fda5517d1
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-10-25 22:47:26 +0300

    update homework4_1

--- src/main/java/com/hillel/shapoval/homeworks/homework4/homework4_1.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework4/homework4_1.java
@@ -4,20 +4,25 @@ public class homework4_1 {
 
     public static void main(String[] args) {
 
-        // Армія Лі "Сила армії Лі = " +
+        // Армія Лі
         int LiWarriorAttack = 13;
         int LiBowmanAttack = 24;
         int LiRiderAttack = 46;
         int LiNumber = 860;
-        System.out.println((LiWarriorAttack * LiNumber) + (LiBowmanAttack * LiNumber) + (LiRiderAttack * LiNumber));
+        System.out.println("Сила армії Лі = " + ((LiWarriorAttack * LiNumber) + (LiBowmanAttack * LiNumber) + (LiRiderAttack * LiNumber)));
 
-        // Армія Мінь "Сила армії Мінь = "
+        // Армія Мінь
         int MinWarriorAttack = 9;
         int MinBowmanAttack = 35;
         int MinRiderAttack = 12;
         double MinNumber = LiNumber * 1.5;
-        System.out.println((MinWarriorAttack * MinNumber) + (MinBowmanAttack * MinNumber) + (MinRiderAttack * MinNumber));
+        System.out.println("Сила армії Мінь = " + ((MinWarriorAttack * MinNumber) + (MinBowmanAttack * MinNumber) + (MinRiderAttack * MinNumber)));
 
 
+      /*
+        int x = 4;
+        int y = 7;
+        System.out.println(y-- + (x++ - ++x) + (x++ - --y) + y++ + --x); // 7 + (4 - 6) + (6 - 6) + 6 + 6 = 7 - 2 + 12 = 17
+       */
     }
 }
