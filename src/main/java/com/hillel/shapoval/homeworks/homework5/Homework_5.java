d01e98d43455ef8efd557760e426fe8e9a0a01f7
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-10-30 16:01:50 +0200

    update version 2.0 howework_5

--- src/main/java/com/hillel/shapoval/homeworks/homework5/Homework_5.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework5/Homework_5.java
@@ -6,69 +6,131 @@ public class Homework_5 {
 
     public static void main(String[] args) {
 
+        String error = "Ви ввели неправильні данні. Значення при введенні фрагів має бути суто числовим. Спробуйте ще раз.";
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введіть імʼя першої команди");
         String nameTeam1 = scanner.nextLine();
 
         System.out.println("Введіть кількість фрагів для першого гравця команди " + nameTeam1);
-        int pl1T1  = scanner.nextInt();
+        int pl1T1 = 0;
+        if (scanner.hasNextInt()) {
+            pl1T1 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         System.out.println("Введіть кількість фрагів для другого гравця команди " + nameTeam1);
-        int pl2T1  = scanner.nextInt();
+        int pl2T1 = 0;
+        if (scanner.hasNextInt()) {
+            pl2T1 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         System.out.println("Введіть кількість фрагів для третього гравця команди " + nameTeam1);
-        int pl3T1  = scanner.nextInt();
+        int pl3T1 = 0;
+        if (scanner.hasNextInt()) {
+            pl3T1 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         System.out.println("Введіть кількість фрагів для четвертого гравця команди " + nameTeam1);
-        int pl4T1  = scanner.nextInt();
+        int pl4T1 = 0;
+        if (scanner.hasNextInt()) {
+            pl4T1 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         System.out.println("Введіть кількість фрагів для пʼятого гравця команди " + nameTeam1);
-        int pl5T1  = scanner.nextInt();
+        int pl5T1 = 0;
+        if (scanner.hasNextInt()) {
+            pl5T1 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
         scanner.nextLine();
+
 
         System.out.println("Введіть імʼя другої команди");
         String nameTeam2 = scanner.nextLine();
 
         System.out.println("Введіть кількість фрагів для першого гравця команди " + nameTeam2);
-        int pl1T2  = scanner.nextInt();
+        int pl1T2 = 0;
+        if (scanner.hasNextInt()) {
+            pl1T2 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         System.out.println("Введіть кількість фрагів для другого гравця команди " + nameTeam2);
-        int pl2T2  = scanner.nextInt();
+        int pl2T2 = 0;
+        if (scanner.hasNextInt()) {
+            pl2T2 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         System.out.println("Введіть кількість фрагів для третього гравця команди " + nameTeam2);
-        int pl3T2  = scanner.nextInt();
+        int pl3T2 = 0;
+        if (scanner.hasNextInt()) {
+            pl3T2 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         System.out.println("Введіть кількість фрагів для четвертого гравця команди " + nameTeam2);
-        int pl4T2  = scanner.nextInt();
+        int pl4T2 = 0;
+        if (scanner.hasNextInt()) {
+            pl4T2 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         System.out.println("Введіть кількість фрагів для пʼятого гравця команди " + nameTeam2);
-        int pl5T2  = scanner.nextInt();
+        int pl5T2 = 0;
+        if (scanner.hasNextInt()) {
+            pl5T2 = scanner.nextInt();
+        } else {
+            System.out.println(error);
+            System.exit(0);
+        }
+
         scanner.nextLine();
 
         final int PLAYER = 5;
 
-        int resultT1 = ((pl1T1 + pl2T1 + pl3T1 + pl4T1 + pl5T1) / PLAYER);
-        int resultT2 = ((pl1T2 + pl2T2 + pl3T2 + pl4T2 + pl5T2) / PLAYER);
+        double resultT1 = ((double) (pl1T1 + pl2T1 + pl3T1 + pl4T1 + pl3T2) / PLAYER);
+        double resultT2 = ((double) (pl1T2 + pl2T2 + pl3T2 + pl4T2 + pl5T2) / PLAYER);
 
-        int result1Fr = (pl1T1 + pl2T1 + pl3T1 + pl4T1 + pl5T1);
-        int result2Fr = (pl1T2 + pl2T2 + pl3T2 + pl4T2 + pl5T2);
+        double result1Fr = (pl1T1 + pl2T1 + pl3T1 + pl4T1 + pl5T1);
+        double result2Fr = (pl1T2 + pl2T2 + pl3T2 + pl4T2 + pl5T2);
 
 
         if (resultT1 > resultT2) {
-            System.out.println("Перемогла команда " + nameTeam1 + " набравши " + result1Fr + " очків.");
+            System.out.println("Перемогла команда " + nameTeam1 + " набравши в сумі " + result1Fr + " очків. " +
+                    "Середній результат гравця команди " + nameTeam1 + " становить " + resultT1 + " очків.");
         }
         if (resultT1 < resultT2) {
-            System.out.println("Перемогла команда " + nameTeam2 + " набравши " + result2Fr + " очків.");
+            System.out.println("Перемогла команда " + nameTeam2 + " набравши в сумі " + result2Fr + " очків. " +
+                    "Середній результат гравця команди " + nameTeam2 + " становить " + resultT2 + " очків.");
+        } else {
+            System.out.println("Перемогла нічия, " + "команда " + nameTeam1 + " набрала " + result1Fr + " очків" +
+                    ", а також команда " + nameTeam2 + " набрала " + result2Fr + " очків. " +
+                    "Середні резульатати гравців команд становлять " + resultT1 + " та " + resultT2 + " очків.");
         }
-        else {
-            System.out.println("Перемогла нічия " + "команда " + nameTeam1 + " набрала " + result1Fr + " очків " +
-                    ", а також команда " + nameTeam2 + " набрала " + result2Fr + " очків.");
-        }
-
-
-
-        //   System.out.println("Перша команда = " + nameTeam1);
-
-
-
-
-//        int number = scanner.nextInt();
-//        System.out.println(number);
-
-
 
 
     }
