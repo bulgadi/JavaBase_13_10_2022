4472ff0ff1c40c947157ae7a4f9a90eae0f1f622
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-11-06 17:15:58 +0200

    update howework_6 tasks_2 is done

--- src/main/java/com/hillel/shapoval/homeworks/homework6/Homework6_2.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework6/Homework6_2.java
@@ -1,8 +1,20 @@
 package com.hillel.shapoval.homeworks.homework6;
-
 public class Homework6_2 {
     //  У Японії числа 4 та 9 вважаються нещасливими.
     //  Вам потрібно пронумерувати 100 космічних шатлів для перевезення людей на Марс так,
     //  щоб у номерах шатлів не траплялося нещасливих чисел. Напишіть функцію,
     //  яка виводить усі номери таких шатлів.
+
+    public static void main(String[] args) {
+        int counter = 1;
+        for (int i = 1; i <= 155; i++) {
+            if (i / 10 == 4 || i / 10 == 9 || i % 10 == 4 || i % 10 == 9 || i / 10 == 14) {
+                continue;
+            }
+            System.out.println(counter);
+            System.out.println("shuttle number: " + i);
+            counter++;
+        }
+    }
 }
+
