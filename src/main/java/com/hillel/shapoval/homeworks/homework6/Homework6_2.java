3bb8486e90e7c6597e23f9766206461dcb2d3205
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-11-06 21:29:50 +0200

    update howework_6 tasks_2 is done

--- src/main/java/com/hillel/shapoval/homeworks/homework6/Homework6_2.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework6/Homework6_2.java
@@ -8,7 +8,7 @@ public class Homework6_2 {
 
     public static void main(String[] args) {
         int counter = 1;
-        for (int i = 1; i <= 155; i++) {
+        for (int i = 1; counter <= 100; i++) {
             if (i / 10 == 4 || i / 10 == 9 || i % 10 == 4 || i % 10 == 9 || i / 10 == 14) {
                 continue;
             }
