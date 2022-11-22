e165d9ff2725521a921cca88ab4b20f564b074fa
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-11-22 17:09:52 +0200

    done howework_9

--- src/main/java/com/hillel/shapoval/homeworks/howework9/Homework9.java
+++ src/main/java/com/hillel/shapoval/homeworks/howework9/Homework9.java
@@ -4,24 +4,49 @@ import java.util.Scanner;
 
 public class Homework9 {
     public static void main(String[] args) {
-
-
         Scanner scanner = new Scanner(System.in);
+        int n = 0;
+        int m = 0;
 
+        do {
+            System.out.println("Введіть M:");
+            if (scanner.hasNextInt()) {
+                m = scanner.nextInt();
+                if (m <= 0) {
+                    System.out.println("Помилка. Введіть додатнє чмсло");
+                }
+            } else {
+                System.out.println("Помилка. Введіть ціле додатнє число");
+            }
+            System.out.println();
+            scanner.nextLine();
+        } while (m <= 0);
 
-        int m = scanner.nextInt();
-        int n = scanner.nextInt();
+        do {
+            System.out.println("Введіть N:");
+            if (scanner.hasNextInt()) {
+                n = scanner.nextInt();
+                if (n <= 0) {
+                    System.out.println("Помилка. Введіть додатнє чмсло");
+                }
+            } else {
+                System.out.println("Помилка. Введіть ціле додатнє число");
+            }
+            System.out.println();
+            scanner.nextLine();
+        } while (n <= 0);
 
-        int[] [] array = new int [m] [n];
-
+        int[][] array = new int[m][n];
+        int[][] array2 = new int[n][m];
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                array [i] [j] = (int) (Math.random() * 10);
+                array[i][j] = (int) (Math.random() * 11);
+                array2[j][i] = array[i][j];
             }
         }
 
-
+        System.out.println("Масив MxN");
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + "\t");
@@ -30,36 +55,14 @@ public class Homework9 {
         }
         System.out.println();
 
-
-
-        for (int i = 0; i < m; i++) {
-            for (int j = i; j < n; j++) {
-                int temp = array[i][j];
-                array[i][j] = array[j][i];
-                array[j][i] = temp;
-            }
-        }
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[j][i] + "\t");
+        System.out.println("Масив NxM");
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                System.out.print(array2[i][j] + "\t");
             }
             System.out.println();
         }
-
-
-
-
-
-
-
-
-
-
-
     }
-
-
 }
 
 
