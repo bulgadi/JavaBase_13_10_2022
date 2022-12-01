c67435386c00437ed33340ee299272001c9cb1e2
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-12-01 13:51:06 +0200

    done howework_10

--- src/main/java/com/hillel/shapoval/homeworks/homework10/Person.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework10/Person.java
@@ -2,31 +2,16 @@ package com.hillel.shapoval.homeworks.homework10;
 
 public class Person {
     public static void main(String[] args) {
-        String address1 = personInfo("Dima", "Shapoval", "Kyiv", "345678");
+        String person1 = personInfo("Dima", "Shapoval", "Kyiv", "345678");
+        String person2 = personInfo("Kate", "Bortson", "Miami", "456789567");
+        String person3 = personInfo("Bob", "Partson", "Washington", "23456789");
 
-
-        String address2 = personInfo("Rfdjhf", "Shapoval", "Kyiv", "345678");
-
-
-        String address3 = personInfo("wgjedhfb", "Shapoval", "Kyiv", "345678");
-
-        System.out.println(address1);
-        System.out.println(address2);
-        System.out.println(address3);
+        System.out.println("a." + person1 + "\n" + "b." + person2 + "\n" + "c." + person3);
 
     }
-
-    static String personInfo(String name1, String surname1, String сity1, String number1){
-        String address1 = "Зателефонувати громадянину " + name1 + " " + surname1 +
-                " із міста " + сity1 + " можна за номером " + number1;
-
-//        String address2 = "Зателефонувати громадянину " + name1 + " " + surname1 +
-//                " із міста " + сity1 + " можна за номером " + number1;
-//
-//        String address3 = "Зателефонувати громадянину " + name1 + " " + surname1 +
-//                " із міста " + сity1 + " можна за номером " + number1;
-        return address1;
-
-
+    static String personInfo(String name, String surname, String сity, String number){
+        String result = "Зателефонувати громадянину " + name + " " + surname +
+                " із міста " + сity + " можна за номером " + number;
+        return result;
     }
 }
