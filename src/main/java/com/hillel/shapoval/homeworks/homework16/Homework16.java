28ed89a66e5147cbb1c0e783948f8eebcd934b94
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-12-30 21:25:53 +0200

    done howework_16

--- src/main/java/com/hillel/shapoval/homeworks/homework16/Homework16.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework16/Homework16.java
@@ -17,8 +17,7 @@ public class Homework16 {
         double average1 = 0;
 
         for (int i = 0; i < a; i++) {
-                average = average1 + mas1[i];
-                average1 = average;
+                average = average + mas1[i];
         }
         average /= mas1.length;
         return average;
