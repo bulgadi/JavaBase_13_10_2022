1e1cf23147af7762b3e5026c6c3d16875a80c73e
Shapoval Dmytro <dima.shapoval@gmail.com>
2022-12-30 21:10:41 +0200

    done howework_16

--- src/main/java/com/hillel/shapoval/homeworks/homework16/Homework16.java
+++ src/main/java/com/hillel/shapoval/homeworks/homework16/Homework16.java
@@ -12,16 +12,15 @@ public class Homework16 {
         }
         return false;
     }
-    public static void arithmeticMean(int[][] mas1, int a, int b) {
+    public static double arithmeticMean(int[] mas1, int a) {
         double average = 0;
         double average1 = 0;
 
         for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                average = average1 + mas1[i][j];
+                average = average1 + mas1[i];
                 average1 = average;
-            }
         }
-        average /= a * b;
+        average /= mas1.length;
+        return average;
     }
 }
